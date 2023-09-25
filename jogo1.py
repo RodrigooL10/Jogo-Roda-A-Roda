c9d3099de6af2c072165dda2accd4829e188dbb8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-25 11:22:47 -0300

    final

--- jogo1.py
+++ jogo1.py
@@ -37,8 +37,14 @@ with open('./palavra.csv', 'r') as f:
     resposta3 = palavras[2]
 
 
-dica = ["_ " * len(palavra) for palavra in palavras]
+dica = []
+for palavra in palavras:
+    dica.append("_ " * len(palavra))
+
 palavras_adivinhadas = [False] * len(palavras)
+
+print("\n-Dicas das palavras: 1º- Um animal; 2º- Um alimento doce; 3º- Uma cor")
+
 
 while "_ " in dica[0] or "_ " in dica[1] or "_ " in dica[2]:
     contador = 0
@@ -46,69 +52,90 @@ while "_ " in dica[0] or "_ " in dica[1] or "_ " in dica[2]:
 
     print(f"\nVez de {nome_jogador}:")
 
+    encontrou = False
+    for d in dica:
+        if d.count("_ ") == 3:
+            encontrou = True
+            break
+        if d.count("_ ") == 2:
+            encontrou = True
+            break
+        if d.count("_ ") == 1:
+            encontrou = True
+            break
+
     #Arriscar chute
-    if any(d.count("_") <= 3 for d in dica):
+    if encontrou:
         for i in range(len(dica)):
             print(f"Palavra {i + 1}: {dica[i]}")
+            
         arriscar = input("Deseja arriscar um palpite? ('S' para arriscar) ").lower()
         if arriscar == 's':
             palpite = input("Dê seu palpite: ")
-            if palpite in palavras and not palavras_adivinhadas[palavras.index(palpite)]:
-                print(f"\n-- Parabéns, {nome_jogador}! Você acertou a palavra! --\n")
-                
-                # Atualize a dica da palavra correta
-                palavra_correta = palavras.index(palpite)
-                dica[palavra_correta] = palpite
-                
-                # Marca a palavra como adivinhada
-                palavras_adivinhadas[palavra_correta] = True
-                
-                #Prêmio ao acertar a palavra, dobra a pontuação
-                pontuacaoJog = nomes['nome' + str(x)]['pontuacao']
-                nova_pontuacao = pontuacaoJog * 2
-                nomes['nome' + str(x)]['pontuacao'] = nova_pontuacao
+            palavra_correta = None
+    
+        # Encontre a posição da palavra no palavras
+        for i, palavra in enumerate(palavras):
+            if palpite == palavra and not palavras_adivinhadas[i]:
+                palavra_correta = i
+                break
 
-                #muda a vez para o próximo
-                if x == 3:
-                    x = 1
-                else:
-                    x += 1
+        if palavra_correta is not None:
+            print(f"\n-- Parabéns, {nome_jogador}! Você acertou a palavra! --\n")
+            
+            # Atualize a dica da palavra correta
+            dica[palavra_correta] = palpite
+            
+            # Marca a palavra como adivinhada
+            palavras_adivinhadas[palavra_correta] = True
+            
+            # Prêmio ao acertar a palavra, dobra a pontuação
+            pontuacaoJog = nomes['nome' + str(x)]['pontuacao']
+            nova_pontuacao = pontuacaoJog * 2
+            nomes['nome' + str(x)]['pontuacao'] = nova_pontuacao
+
+            #muda a vez para o próximo
+            if x == 3:
+                x = 1
+            else:
+                x += 1
 
                 #mostra o placar atualizado
-                print("----- Placar -----")
-                for jogador, info in nomes.items():
-                    nome = info['nome']
-                    pontuacao = info['pontuacao']
-                    print(f"{nome}\t\t{pontuacao}")
+            print("----- Placar -----")
+            for jogador, info in nomes.items():
+                nome = info['nome']
+                pontuacao = info['pontuacao']
+                print(f"{nome}\t\t{pontuacao}")
 
-                #exibe o nome do próximo jogador
-                nome_jogador = nomes['nome' + str(x)]['nome']
-                print(f"\nVez de {nome_jogador}:")
+            #exibe o nome do próximo jogador
+            nome_jogador = nomes['nome' + str(x)]['nome']
+            print(f"\nVez de {nome_jogador}:")
 
             #se errar o palpite, jogador perde tudo
+        else:
+            print(f"{nome_jogador} errou o palpite, perdeu tudo!\n")
+            nomes['nome' + str(x)]['pontuacao'] = 0
+
+            #muda a vez para o próximo jogador
+            if x == 3:
+                x = 1
             else:
-                print(f"{nome_jogador} errou o palpite, perdeu tudo!\n")
-                nomes['nome' + str(x)]['pontuacao'] = 0
+                x += 1
 
-                #muda a vez para o próximo jogador
-                if x == 3:
-                    x = 1
-                else:
-                    x += 1
+            #exibe o placar atualizado
+            print("----- Placar -----")
+            for jogador, info in nomes.items():
+                nome = info['nome']
+                pontuacao = info['pontuacao']
+                print(f"{nome}\t\t{pontuacao}")
 
-                #exibe o placar atualizado
-                print("----- Placar -----")
-                for jogador, info in nomes.items():
-                    nome = info['nome']
-                    pontuacao = info['pontuacao']
-                    print(f"{nome}\t\t{pontuacao}")
-
-                #exibe o nome do próximo jogador
-                nome_jogador = nomes['nome' + str(x)]['nome']
-                print(f"\nVez de {nome_jogador}:")
+            #exibe o nome do próximo jogador
+            nome_jogador = nomes['nome' + str(x)]['nome']
+            print(f"\nVez de {nome_jogador}:")
+    
 
     #se todas as palavras forem adivinhadas, determina fim do loop while
-    if all(palavras_adivinhadas):
+    if palavras_adivinhadas[0] == True and palavras_adivinhadas[1] == True and palavras_adivinhadas[2] == True:
         break
 
     # Seleciona o valor da roda
@@ -123,7 +150,8 @@ while "_ " in dica[0] or "_ " in dica[1] or "_ " in dica[2]:
 
     #Jogador perde tudo
     if select == 0:
-        print("\n\n--- Jogador Perdeu tudo! ---\n\n")
+        nome_jogador = nomes['nome' + str(x)]['nome']
+        print(f"\n\n--- {nome_jogador} Perdeu tudo! ---\n\n")
         nomes['nome' + str(x)]['pontuacao'] = 0
         if x == 3:
             x = 1
@@ -132,7 +160,8 @@ while "_ " in dica[0] or "_ " in dica[1] or "_ " in dica[2]:
     
     #Jogador passa a vez
     elif select == 1:
-        print("\n\n--- Passou a vez! ---\n\n")
+        nome_jogador = nomes['nome' + str(x)]['nome']
+        print(f"\n\n--- {nome_jogador} Perdeu tudo! ---\n\n")
         if x == 3:
             x = 1
         else:
@@ -177,12 +206,22 @@ while "_ " in dica[0] or "_ " in dica[1] or "_ " in dica[2]:
             x += 1
         
     
-    # Verifica se todas as palavras foram adivinhadas e remove as adivinhadas
-    for i in range(len(palavras) - 1, -1, -1):
-        if palavras_adivinhadas[i]:
-            del palavras[i]
-            del dica[i]
-            del palavras_adivinhadas[i]
+# Verifica se todas as palavras foram adivinhadas e remove as adivinhadas
+novas_palavras = []
+novas_dica = []
+novas_palavras_adivinhadas = []
+
+# Itere pelas listas originais e copie apenas os elementos não adivinhados para as novas listas
+for i in range(len(palavras)):
+    if not palavras_adivinhadas[i]:
+        novas_palavras.append(palavras[i])
+        novas_dica.append(dica[i])
+        novas_palavras_adivinhadas.append(palavras_adivinhadas[i])
+
+# Atualize as listas originais com as novas listas
+palavras = novas_palavras
+dica = novas_dica
+palavras_adivinhadas = novas_palavras_adivinhadas
 
 print("----- Placar Final -----")
 for jogador, info in nomes.items():
